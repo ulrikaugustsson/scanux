8c4fa4e2f4721b174004bc7a6d24f9396d7ab7e8
Ulrik Augustsson <ulrikaugustsson@gmail.com>
2015-10-10 10:06:45 +0200

    Release 0.3.1

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 import Rx from 'rx';
 
-export default function (initalState = {}) {
-	const masterStore = new Rx.BehaviorSubject(initalState);
-	const dispatcher = new Rx.Subject();
-	const actions = {};
+const masterStore = new Rx.BehaviorSubject({});
+const dispatcher = new Rx.Subject();
+const actions = {};
 
-	return {
-		registerReducer: (actionName, reducer, initalState = {}) => {
-			dispatcher
-				.filter(obj => {
-					return obj && obj.actionName === actionName
-				})
-				.map(obj => obj.observable)
-				.flatMap(observable => observable)
-				.scan(reducer, initalState)
-				.subscribe(newState => {
-					masterStore
-						.onNext(newState);
-				});
-		},
-
-		listen: (callback, errorHandler, onComplete) => {
-			return masterStore
-				.scan((prev, next) => {
-					return {...prev, ...next};
-				}, {})
-				.subscribe(callback, errorHandler, onComplete);
-		},
-
-		addAction: (actionName, action) => {
-			actions[actionName] = action;
-		},
-
-		triggerAction: (actionName, data) => {
-			dispatcher.onNext({
-				actionName, observable: actions[actionName](data)
+const scanux = {
+	registerReducer: (actionName, reducer, initalState = {}) => {
+		dispatcher
+			.filter(obj => {
+				return obj && obj.actionName === actionName
+			})
+			.map(obj => obj.observable)
+			.flatMap(observable => observable)
+			.scan(reducer, initalState)
+			.subscribe(newState => {
+				masterStore
+					.onNext(newState);
 			});
-		}
+	},
+
+	listen: (callback, errorHandler, onComplete) => {
+		return masterStore
+			.scan((prev, next) => {
+				return {...prev, ...next};
+			}, {})
+			.subscribe(callback, errorHandler, onComplete);
+	},
+
+	addAction: (actionName, action) => {
+		actions[actionName] = action;
+	},
+
+	triggerAction: (actionName, data) => {
+		dispatcher.onNext({
+			actionName, observable: actions[actionName](data)
+		});
 	}
 }
+
+export default scanux;
